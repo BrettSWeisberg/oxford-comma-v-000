c109c5c83ad57f7f89bea85ace77f5f4878480dd
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-28 02:12:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,10 @@ if array.length == 2
     array.join(" and ")
 elsif array.length == 1
     array.join
-elsif array.length == 3
+else array.length > 2  
   array[-1].insert(0, "and ")
  array.join( ", " )
+end
+end
 
-else
-    array[-1].insert(0, "and ")
-   array.join( ", " )
-end
-end
+
